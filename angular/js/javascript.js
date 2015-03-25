5a465947e8e9d4aa859109ace25776f55cf038d9
aresavazquez <ares _vb>
2015-03-24 19:08:49 -0600

    js

--- angular/js/javascript.js
+++ angular/js/javascript.js
@@ -1,4 +1,15 @@
-module.controller ('', [''])
-.controller ('controller', function(){
-	
+angular.module ("AutoData", ['ngSanitize', 'ngMaterial'])
+.controller ('NameController', function(){
+	var self = this;
+	this.ButtonEnviar = {};
+	this.Input = function(){
+		$http.get ("http://api.labcd.mx/v1/movilidad/vehiculos/", {})
+		.success (function(data){
+			self.datos = data;
+		})
+		.error (function(){
+			alert ('Error! los datos no se cargaron correctamente')
+		});
+	};
+
 });
